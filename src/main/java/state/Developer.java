d8da9fa9dabefbf0ee44b4135060267da244b81b
gchursov <topsycreed@gmail.com>
2023-11-07 23:41:01 +0100

    Added state examples

---
+++ src/main/java/state/Developer.java
@@ -0,0 +1,23 @@
+package state;
+
+public class Developer {
+    Activity activity;
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void changeActivity() {
+        if (activity instanceof Sleeping) {
+            setActivity(new Training());
+        } else if (activity instanceof Training) {
+            setActivity(new Coding());
+        } else if (activity instanceof Coding) {
+            setActivity(new Sleeping());
+        }
+    }
+
+    public void startActivity() {
+        activity.startActivity();
+    }
+}
---
+++ src/main/java/state/Training.java
@@ -0,0 +1,8 @@
+package state;
+
+public class Training implements Activity{
+    @Override
+    public void startActivity() {
+        System.out.println("Training...");
+    }
+}
